ad15041f42cb5815415d395acb945f246035edcb
Patrick Nguyen <patricknguyen002@gmail.com>
2019-04-17 12:47:39 -0700

    Test

---
+++ app.js
@@ -0,0 +1,104 @@
+const hbs = require('hbs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const geocode = require('./backend.js');
+var port = process.env.PORT || 8080;
+// var port = 8080;
+//middleware
+var route = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views');
+route.use(express.static(__dirname + '/public'));
+route.set('view engine', 'hbs');
+route.use(bodyParser.json());
+route.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+route.get('/', (request,response) => {
+
+    try{
+
+        response.render('index', {
+            jumbo_main: "Welcome",
+            jumbo_sec: "Image Parser",
+        })
+
+    }catch(err){
+        if (err){
+            response.render('404',{
+                message: "Could not connect"
+            })
+        }
+    }
+
+});
+
+route.get('/api_1', async(request,response)=> {
+    try{
+        response.render('api_1',{
+            jumbo_main: "Enter a number"
+        })
+    }catch(err) {
+        if (err){
+            response.render('404')
+        }
+    }
+
+});
+
+route.post('/get_deck', async(request, response)=> {
+    try{
+        var entry = request.body.deck_entry;
+        const code = await geocode.getDeck(entry);
+        console.log(code[1].image);
+
+        var deck_list = [];
+        for (var i=0; i< code.length; i++){
+            deck_list.push({image: code[i].image})
+        }
+        response.render('api_1',{
+            jumbo_main: "Deck",
+            jumbo_sec: "Build a deck",
+            url: deck_list
+        });
+    }catch (err){
+        if (err){
+            response.render('api_1', {
+                jumbo_main: "Deck App",
+                jumbo_sec: "Could not connect to server."
+            })
+        }
+        }
+
+
+});
+
+route.post('/get_image', async(request, response)=> {
+    try{
+        var entry = request.body.image_entry;
+        var imageapi = await geocode.getImage(entry);
+        var images = [];
+
+        for (var i=0; i<imageapi.length; i++){
+            images.push({path: imageapi[i].links[0].href});
+        }
+        response.render('index', {
+            jumbo_main: "Welcome",
+            jumbo_sec: "Image Parser",
+            url: images
+        })
+    }catch(err){
+        if (err){
+            response.render('index', {
+                jumbo_main: "Welcome",
+                jumbo_sec: err
+            })
+        }
+    }
+});
+
+route.listen(port, (request, response) => {
+    console.log(`Server is up on port ${port}`)
+});
--- backend.js
+++ backend.js
@@ -1,28 +1,32 @@
 const request = require('request');
 const axios = require('axios');
 
-var getDeck = async(deck_count) => {
+
+var getDeck = async(deck_entry) => {
     try{
-        const deck = await axios.get(`https://deckofcardsapi.com/api/deck/new/draw/?count=${deck_count}`)
+        const deck = await axios.get(`https://deckofcardsapi.com/api/deck/new/draw/?count=${deck_entry}`);
         return deck.data.cards
-    }catch(e){
-        if(e){
-            throw("Error deck")
+    }catch(err){
+        if (err){
+            throw("Error placeholder deck")
         }
     }
 };
 
-var getImage = async(search)=>{
+var getImage = async(search_entry)=>{
     try{
-        const images = await axios.get(`https://images-api.nasa.gov/search?q=${search}`)
+        const images = await axios.get(`https://images-api.nasa.gov/search?q=${search_entry}`);
         return images.data.collection.items
-    }catch(e) {
-        if (e) {
-            throw("Error")
+    }catch(error){
+        //status codes 200 ok, 400 bad request, 404 not found, 500 server error
+        if (error){
+            throw ("Error placeholder")
         }
     }
 };
 
+
+//Image API end
 module.exports = {
     getDeck,
     getImage
