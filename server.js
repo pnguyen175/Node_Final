c587924b6d8674da705a904184730530a463f1f6
Patrick Nguyen <patricknguyen002@gmail.com>
2019-04-16 19:37:25 -0700

    First Commit

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 8080;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('message', (text) => {
+    return text.toUpperCase();
+});
+
+app.use((request, response, next) => {
+    var time = new Date().toString();
+    // console.log(`${time}: ${request.method} ${request.url}`);
+    var log = (`${time}: ${request.method} ${request.url}`);
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if (error) {
+            console.log('Unable to log message');
+        }
+    });
+    next();
+});
+
+// app.use((request, response, next) => {
+//     response.render('maintenence.hbs', {
+//         title: 'Maintenance'
+//     })
+// });
+
+app.get('/', (request, response) => {
+    // response.send('<h1>Hello Express!</h1>');
+    response.send({
+        name: 'Your Name',
+        school: [
+            'BCIT',
+            'UBC',
+            'SFU',
+            'Douglas',
+            'UVIC',
+            'UofT',
+            'Waterloo',
+            'Emily Carr'
+        ]
+    })
+});
+
+app.get('/info', (request, response) => {
+    response.render('about.hbs', {
+        title: 'About page',
+        year: new Date().getFullYear(),
+        welcome: 'Hello!'
+    });
+});
+
+
+app.get('/404', (request, response) => {
+    response.send({
+        error: 'Page not found'
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on the port ${port}`)
+});
